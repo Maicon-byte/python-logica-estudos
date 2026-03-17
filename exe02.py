1354a361547f6694a2b8b72c45900c80d1f6db1c
Maicon Silva de Sousa <83846236+Maicon-byte@users.noreply.github.com>
2026-03-17 00:57:27 -0300

    Update exe02.py
    Eu deixei o código mais simples.

--- exe02.py
+++ exe02.py
@@ -1,18 +1,14 @@
-nome = None
-cidade = None
-idade = None
-profissao = None
 ano = 2026
 
 nome = input("Qual o seu nome?")
 idade = int(input("Qual a sua idade"))
 cidade = input("Em que cidade você mora?")
 profissao = input("Qual sua Profissão?")
-ano_nacimento = ano - idade
+ano_nacismento = ano - idade
 
 print("Olá meu nome é", nome)
 print("Eu tenho", idade, "anos")
 print("Moro em", cidade)
 print("Trabalho como", profissao)
-print("Eu naci em", ano_nacimento)
+print("Eu nasci em", ano_nacismento)
 
